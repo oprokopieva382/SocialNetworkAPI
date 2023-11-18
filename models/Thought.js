6fcb6e74b4c4022a90a8171baf3a0547d08809b0
Oksana(KsyFate) <oprokopieva382@gmail.com>
2023-11-17 23:03:01 -0600

    add Thougt.js with schema

---
+++ models/Thought.js
@@ -0,0 +1,36 @@
+const { Schema, model } = require("mongoose");
+const dayjs = require("dayjs");
+const reactionSchema = require("./Reaction");
+
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      minlength: 1,
+      maxlength: 280,
+    },
+    createdAt: {
+      type: Date,
+      default: dayjs().toDate(),
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    reactions: [reactionSchema], 
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+  }
+);
+
+thoughtSchema.virtual("reactionCount").get(function () {
+  return this.reactions.length;
+});
+
+const Thought = model("thought", thoughtSchema);
+
+module.exports = Thought;
