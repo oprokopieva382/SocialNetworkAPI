01eb03675008d79ab0e9f4016bf22ffdbc3124bf
Oksana(KsyFate) <oprokopieva382@gmail.com>
2023-11-17 23:14:50 -0600

    add Reaction.js with schema

---
+++ models/Reaction.js
@@ -0,0 +1,36 @@
+const { Schema, Types } = require("mongoose");
+const dayjs = require("dayjs");
+
+const reactionSchema = new Schema(
+  {
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId(),
+    },
+    reactionBody: {
+      type: String,
+      required: true,
+      maxlength: 280,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: dayjs().toDate(),
+    },
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+reactionSchema.virtual("formattedCreatedAt").get(function () {
+  return dayjs(this.createdAt).format("YYYY-MM-DD HH:mm:ss");
+});
+
+module.exports = reactionSchema;
