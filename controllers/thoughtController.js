c99f85a3af5f54cc0668b8a190fbb02c21e9ba11
Oksana(KsyFate) <oprokopieva382@gmail.com>
2023-11-18 19:45:29 -0600

    add getSingleThought function with logic

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -10,3 +10,23 @@ const getAllThoughts = async (req, res) => {
     res.status(500).json({ error: "Internal Server Error" });
   }
 };
+
+// GET a single thought by _id
+const getSingleThought = async (req, res) => {
+  const { thoughtId } = req.params;
+  try {
+    const thought = await Thought.findById(thoughtId).populate("reactions");
+
+    thought
+      ? res.status(200).json(thought)
+      : res.status(404).json({ message: "Thought not found" });
+  } catch (err) {
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
+
+module.exports = {
+  getAllThoughts,
+  getSingleThought,
+};
