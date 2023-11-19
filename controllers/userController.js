0708cf6a33d6554fa444a9e71a7d33ee213dfdfe
Oksana(KsyFate) <oprokopieva382@gmail.com>
2023-11-18 19:08:22 -0600

    add getSingleUser function with logic

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,12 +4,26 @@ const { User } = require("../models");
 // GET all users
 const getAllUsers = async (req, res) => {
   try {
-    const users = await User.find()
-    res.json(users)
+    const users = await User.find();
+    res.json(users);
   } catch (err) {
     res.status(500).json({ error: "Internal Server Error" });
   }
 };
 
+// GET a single user by _id and populated thought and friend data
+const getSingleUser = async (req, res) => {
+  const { userId } = req.params;
+  try {
+    const user = await User.findById(userId)
+      .populate("thoughts")
+      .populate("friends");
 
-module.exports = { getAllUsers };
+    user ? res.json(user) : res.status(404).json({ message: "User not found" });
+
+  } catch (err) {
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
+module.exports = { getAllUsers, getSingleUser };
